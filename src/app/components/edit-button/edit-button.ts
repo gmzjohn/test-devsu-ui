6ec28a4bfeebcee0cfdbe24f42bc87966f4de57c
John Gomez <gomezjohn@tuta.com>
2026-05-09 10:52:19 -0400

    Create Edit Button
    Reusable edit button was created to be used in the tables

---
+++ src/app/components/edit-button/edit-button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-button.html',
+  styleUrl: './edit-button.css',
+})
+export class EditButton { }
