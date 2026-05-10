6d09f6593806c74868e672c539be1bc9322d6e10
John Gomez <gomezjohn@tuta.com>
2026-05-10 11:47:30 -0400

    Create Env Variable
    Create an env variable to be reused in other services other than Client

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface Client {
   id: number;
@@ -19,7 +20,7 @@ export interface Client {
 })
 export class ClientService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/api/clients';
+  private apiUrl = `${environment.apiUrl}/clients`;
 
   getClients(): Observable<Client[]> {
     return this.http.get<Client[]>(this.apiUrl);
